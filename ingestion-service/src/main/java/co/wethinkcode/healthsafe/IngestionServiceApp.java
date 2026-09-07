de64721f42ac3fd22b858f224267ff381653212c
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2026-09-07 17:39:16 +0200

    update: adding basic hashmap logic

--- ingestion-service/src/main/java/co/wethinkcode/healthsafe/IngestionServiceApp.java
+++ ingestion-service/src/main/java/co/wethinkcode/healthsafe/IngestionServiceApp.java
@@ -4,6 +4,7 @@ import io.javalin.Javalin;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class IngestionServiceApp {
 
@@ -20,18 +21,23 @@ public class IngestionServiceApp {
         String csvSplitBy = ",";
 
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            HashMap<String, String> map = new HashMap<>();
 
             while ((line = br.readLine()) != null) {
                 String[] data = line.trim().split(csvSplitBy);
 
                 if (data.length > 0) {
                     for (int i = 0; i < data.length; i++) {
-                        data[i] = data[i].trim().toUpperCase();
+                        data[i] = data[i].trim().toLowerCase();
                     }
                     System.out.println("Ward ID: " + data[0]);
                     System.out.println("Wing: " + data[1]);
-                    System.out.println("Department: " +data[2]);
-                    System.out.println("Beds Available: " + data[3]);
+                    System.out.println("Department: " + data[2]);
+                    map.put("ward_id", data[0]);
+                    map.put("wing", data[1]);
+                    map.put("department", data[2]);
+                    map.put("beds_available", data[3]);
+                    // System.out.println("Beds Available: " + data[3]);
                 }
             }
         } catch (IOException e) {
