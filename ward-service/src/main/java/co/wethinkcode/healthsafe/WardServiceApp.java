e71d4f2826736fdebcd209105fe1dd4ed45715be
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2026-09-08 15:28:17 +0200

    fix: removed wrong Queue import

--- ward-service/src/main/java/co/wethinkcode/healthsafe/WardServiceApp.java
+++ ward-service/src/main/java/co/wethinkcode/healthsafe/WardServiceApp.java
@@ -11,7 +11,6 @@ import java.net.http.HttpResponse;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Queue;
 import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.core.type.TypeReference;
